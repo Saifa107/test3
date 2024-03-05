6357b96ca31414b7bbcfd60d00abef31f0e5dac3
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2024-03-05 22:34:30 +0700

    add table

--- src/app/main/member/member.component.ts
+++ src/app/main/member/member.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-member',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './member.component.html',
   styleUrl: './member.component.scss'
 })
